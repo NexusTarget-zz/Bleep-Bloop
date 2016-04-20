b865df5368bd20c29ba07edc5ba5738814d9d07c
Jack Moran <Jackmoran50@gmail.com>
2016-04-20 20:36:29 +1200

    Update networking.cpp

--- networking.cpp
+++ networking.cpp
@@ -2,20 +2,20 @@
 #include <time.h>
 
 extern "C" int init(int d_lev);
-extern "C" int connect_to_server( char server_addr[15], int port);
-extern "C" int send_to_server(char password[24]);
-extern "C" int receive_from_server(char password[24]);
+extern "C" int connect_to_server(char server_addr[15], int port);
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
 
 int main (){
    // This sets up the RPi hardware and ensures
    // everything is working correctly
    init(0);
-   connect_to_server("###.###.###.###", ####); //connects to server with the ip address 130.195.6.196 on port 1024
+   connect_to_server("###.###.###.###", ####); //connects to server with the ip address and port
    send_to_server("Please"); //sends please to the connected server
-   char password[24];
-   receive_from_server(password); //Password from the connected server
-   printf("%s/n", password);
-   send_to_server(password);
+   char message[24];
+   receive_from_server(message); //Password from the connected server
+   printf("%s/n", message);
+   send_to_server(message);
    printf("%s/n", "Password was sent to the server");
    return 0;
 }
