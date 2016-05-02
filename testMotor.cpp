335dae2e8e6c575abb812ebc9ed2f413d5a5e935
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-02 17:03:50 +1200

    Comment updates
    More accurate descriptions of the code.

--- testMotor.cpp
+++ testMotor.cpp
@@ -6,19 +6,17 @@ extern "C" int Sleep( int sec , int usec );
 extern "C" int set_motor( int motor , int speed);
 
 int main (){
-   // This sets up the RPi hardware and ensures
-   // everything is working correctly
+   // This sets up the RPi hardware and ensures everything is working correctly.
    init(0);
-   // Sets the motor connected to pin 1 and pin 2 to rotate
-   // in one direction at MAX speed .
+   // Sets the motor connected to pin 1 and pin 2 to rotate in the forward direction at half speed for 3 seconds.
    set_motor(1, 127);
    set_motor(2, 127);
    Sleep (3 ,000000);
-   // Sets the motor connected to pin 1 and pin 2 to rotate
-   // in the opposite direction at 100% speed .
+   // Sets the motor connected to pin 1 and pin 2 to rotate in the reverse direction at half speed for 3 seconds.
    set_motor(1, -127);
    set_motor(2, -127);
    Sleep(3 ,000000);
+   // Stops both motors.
    set_motor(1, 0);
    set_motor(2, 0);
    return 0;
