d315eb648dd926f5848d6ce9a2d3c5017097a144
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-13 02:32:49 +1200

    Recoded the calculation for proportional #experimental
    Very experimental code for calculating the proportional. If it works, i believe it will be far more accurate and reliable than previously, and some others teams.
    Made some changes to the timestep, kp and kp values. 
    New method for calculating the error value, is experimental but should be implemented into the currently tested code whether my new code works or not.
    
    //Reason for the experimental\\
    After all the stuff we have done in lectures and labs, i decided that i would try write some code that better reflected trying to ignore noise and gain more accurate and reliable readings.
    Starts off with a for loop that runs 10 times, each time it takes a picture and then creates a total value for each pixel for each photo.
    Takes the average of each pixel, this will dramatically help with reducing noise and makes the pixel information more reliable.
    it then checks if each pixel average is above 127, this is half brightness and im assuming that black and white will be significantly lower or higher than this value respectively.
    Other than that the code is unchanged.

--- testLineFollow.cpp
+++ testLineFollow.cpp
@@ -15,7 +15,7 @@ int main(){
 	// This sets up the RPi hardware and ensures
 	// everything is working correctly
 	int pLine[32]; //Creates an array to store pixel values
-	int sampleSize = sizeof(pLine); //A set variable of the arrays length
+	int sampleSize = sizeof(pLine)/sizeof(int); //A set variable of the arrays length
 	int pTot; //Total pixel value
 	float avg; //The average brightness of pixels
 	float errorValue; //Error value that sets the distance between line and centre
@@ -23,33 +23,52 @@ int main(){
 	float dErrorValue; //Error value used by the derivative value kd
 	int left; 
 	int right;
-	double kp = 4.0; //P value in PD controller
+	double kp = 0.2; //P value in PD controller
 	double kd = 4.0; //D value in PD controller
-	double timeStep = 1.0; //The time period used for calculating kp
+	double timeStep = 0.2; //The time period used for calculating kp
 	time_t start_t; //The start point for calculating a time difference
 	time_t end_t = 0; //End point for calculating time difference
+	int repitition = 10;
 
 	open_screen_stream(); //Allows the camera to be displayed on the desktop
 
 	while(true){ //This creates a never ending loop
-		pTot = 0;
 		errorValue = 0;
+		/**
+		pTot = 0;
 		avg = 0;
-
 		take_picture(); //Self explanatory
-
 		for (int i = 0; i < sampleSize; i++){ //Finds brightness of each required pixel
 			pLine[i] = get_pixel(i*10,120,3);
 			pTot += pLine[i];
 		}
-
 		avg = (float)pTot/sampleSize; //Gets average brightness of pixels
-
+		
 		for (int i = 0; i < sampleSize; i++){ //If pixel is brighter than average, negative number means line is to the left, positive if line is to the right
 			if (pLine[i]>avg){
 				errorValue += 10*(i-sampleSize/2);
 			}
 		}
+*/
+
+//*************************EXPERIMENTAL**************************
+		for (int r = 0; r < repitition; r++)//takes 10 photos, storing the total value for each pixel
+		{
+			take_picture(); //Self explanatory
+			for (int i = 0; i < sampleSize; i++){ //Finds brightness of each required pixel
+				pLine[i] += get_pixel(i*10,120,3);
+			}
+		}
+		for (int i = 0; i < sampleSize; i++){ //Finds the average brightness of each required pixel
+				pLine[i] = pLine[i]/repitition
+			}
+		for (int i = 0; i < sampleSize; i++){ //Checks if the pixel is brighter than half brigtness (making an assumption that black will be consstantly les and white consistantly more)
+			if (pLine[i]>127){
+				errorValue += 10*(i-sampleSize/2);//adds a positive value if the line is to the right, negative value if line is to the left
+			}
+		}
+//*******************************END OF EXPERIMENTAL******************************
+		
 		time(&start_t); //Finds the current time
 		if (difftime(end_t, start_t) > timeStep) //Runs if the time period is larger than the timestep
 		{
@@ -61,7 +80,7 @@ int main(){
 			prevErrorValue = errorValue;
 		}
 
-		errorValue = errorValue/sampleSize; //Gets average of error
+
 		printf("%f\n", errorValue); //%f because errorValue is a float
 		// Determines the new motor speeds to alter direction
 		left = 40 + errorValue * kp + dErrorValue *kd;
