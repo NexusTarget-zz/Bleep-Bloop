0f47aaf6619809a4b277031dd5b4a17b1114e7a2
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-24 17:02:08 +1200

    turns for a certain amount of time at intersection

--- code/testLineFollow.cpp
+++ code/testLineFollow.cpp
@@ -94,18 +94,15 @@ int main()
 		{
 			if(errorValue >= 0 || pixelCount == 32) //if line not found or left 90deg corner/T junction detected turn left
 			{
-			/**	set_motor(1, -80); 
-				set_motor(2, -80);
-				Sleep(0, 5000); */
-				set_motor(1, motorSpeed);
-				set_motor(2, -1*motorSpeed);
-			}else if(errorValue < 0) 	//if right hand 90deg corner found, turn right
-			{
-			/**	set_motor(1, -80);
-				set_motor(2, -80);
-				Sleep(0, 5000); */
-				set_motor(1, -1*motorSpeed);
+				set_motor(1, motorSpeed/4);
 				set_motor(2, motorSpeed);
+				sleep(1, 00000);
+			}
+			else if(errorValue < 0) 	//if right hand 90deg corner found, turn right
+			{
+				set_motor(1, motorSpeed);
+				set_motor(2, motorSpeed/4);
+				sleep(1, 00000);
 			}
 		}
 		else if(!lineFound)
