7caa398c389e56b64f17383e8da8a09643d06277
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-24 12:01:56 +1200

    kp = 6

--- code/testLineFollow.cpp
+++ code/testLineFollow.cpp
@@ -23,7 +23,7 @@ int main(){
 	float errorTot = 0;
 	int left; 
 	int right;
-	double kp = 7.5; //P value in PD controller
+	double kp = 6; //P value in PD controller
 	double kd = 0; //D value in PD controller
 	double ki = 0;
 	double timeStep = 0.2; //The time period used for calculating kp
