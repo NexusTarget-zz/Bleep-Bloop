d6ccc38d8ce5a90f6eef4710224df951c130224b
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-24 12:46:36 +1200

    kp = 50

--- code/testLineFollow.cpp
+++ code/testLineFollow.cpp
@@ -24,7 +24,7 @@ int main()
 	float errorTot = 0;
 	int left; 
 	int right;
-	double kp = 10; //P value in PD controller
+	double kp = 50; //P value in PD controller
 	double kd = 0; //D value in PD controller
 	double ki = 0;
 	double pixelCount;
