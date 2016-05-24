e55711bf13e9d7f94f3bb650fb81bb26f55858b1
finn-wa <finnwa24@gmail.com>
2016-05-24 14:46:07 +1200

    prints out IR sensor reading in an infinite loop

---
+++ code/testIR.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+//extern "C" int InitHardware(); //used this on assignment 5??
+extern "C" int init(int d_lev);
+extern "C" int ReadAnalog(int ch_adc);
+extern "C" int Sleep(int sec, int usec);
+
+int main(){
+  
+  init(0);
+  //InitHardware();
+  int adc_reading;
+  while(true)
+    {
+      adc_reading = ReadAnalog(0);
+      printf("%d\n", adc_reading);
+      Sleep(0,500);
+    }
+
+}
