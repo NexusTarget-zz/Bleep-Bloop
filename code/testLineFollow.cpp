4d8a8d0a0df6a4b4b779798f56aa757802d8e0d5
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-24 17:56:15 +1200

    pixel count threshhold 18

--- code/testLineFollow.cpp
+++ code/testLineFollow.cpp
@@ -90,18 +90,18 @@ int main()
 			printf(" --- Centre\n");
 		}
 		
-		if(pixelCount >= 28)
+		if(pixelCount >= 18)
 		{
 			if(errorValue >= 0 || pixelCount == 32) //if line not found or left 90deg corner/T junction detected turn left
 			{
-				set_motor(1, motorSpeed/4);
-				set_motor(2, motorSpeed);
+				set_motor(1, motorSpeed);
+				set_motor(2, motorSpeed/4);
 				Sleep(0, 50000);
 			}
 			else if(errorValue < 0) 	//if right hand 90deg corner found, turn right
 			{
-				set_motor(1, motorSpeed);
-				set_motor(2, motorSpeed/4);
+				set_motor(1, motorSpeed/4);
+				set_motor(2, motorSpeed);
 				Sleep(0, 50000);
 			}
 		}
