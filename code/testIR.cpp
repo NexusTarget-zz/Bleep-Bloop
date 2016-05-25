bafc2533a743b33450962b27f827bf9a26ed712d
finn-wa <finnwa24@gmail.com>
2016-05-25 15:43:07 +1200

    test sensor 2

--- code/testIR.cpp
+++ code/testIR.cpp
@@ -13,7 +13,7 @@ int main(){
   int adc_reading;
   while(true)
     {
-      adc_reading = read_analog(1);
+      adc_reading = read_analog(2);
       printf("%d\n", adc_reading);
       Sleep(0,500);
     }
