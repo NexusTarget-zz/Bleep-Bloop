b5ced949a4eb90a41d09b5ad87534100c381c2aa
finn-wa <finnwa24@gmail.com>
2016-05-25 15:49:53 +1200

    tests all sensors at once

--- code/testIR.cpp
+++ code/testIR.cpp
@@ -13,9 +13,13 @@ int main(){
   int adc_reading;
   while(true)
     {
+      adc_reading = read_analog(0);
+      printf("Front: %d", adc_reading);
+      adc_reading = read_analog(1);
+      printf("Left: %d", adc_reading);
       adc_reading = read_analog(2);
-      printf("%d\n", adc_reading);
-      Sleep(0,500);
+      printf("Right: %d\n", adc_reading);
+      Sleep(0,2500);
     }
 
 }
