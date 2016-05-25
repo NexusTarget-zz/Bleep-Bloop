bfacc4b0a777ad3829a7ce1088c7ea3fe7059177
finn-wa <finnwa24@gmail.com>
2016-05-25 12:40:26 +1200

    testing github downloading

--- code/testIR.cpp
+++ code/testIR.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <time.h>
 
-extern "C" int InitHardware(); //used this on assignment 5??
-//extern "C" int init(int d_lev);
+//extern "C" int InitHardware(); //used this on assignment 5??
+extern "C" int test(int d_lev);
 extern "C" int ReadAnalog(int ch_adc);
 extern "C" int Sleep(int sec, int usec);
 
 int main(){
   
- // init(0);
-  InitHardware();
+ test(0);
+  //InitHardware();
   int adc_reading;
   while(true)
     {
