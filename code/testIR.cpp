9efdc9e33180baeb50ac949ee695f926df4d02c2
finn-wa <finnwa24@gmail.com>
2016-05-27 01:30:16 +1200

    removed commented out code

--- code/testIR.cpp
+++ code/testIR.cpp
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 
-//extern "C" int InitHardware(); //used this on assignment 5??
 extern "C" int init(int d_lev);
 extern "C" int read_analog(int ch_adc);
 extern "C" int Sleep(int sec, int usec);
 
 int main(){
   
- init(0);
-  //InitHardware();
+  init(0);
   int adc_reading;
   while(true)
     {
