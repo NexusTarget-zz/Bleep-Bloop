670db6e0f7a2a5e4cedeff406b035c2931ac7917
Lenton <lentonneale@gmail.com>
2016-05-27 02:35:54 +0000

    testIR changes

--- code/testIR.cpp
+++ code/testIR.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <time.h>
 
-//extern "C" int InitHardware(); //used this on assignment 5??
 extern "C" int init(int d_lev);
 extern "C" int read_analog(int ch_adc);
 extern "C" int Sleep(int sec, int usec);
@@ -9,17 +8,16 @@ extern "C" int Sleep(int sec, int usec);
 int main(){
   
  init(0);
-  //InitHardware();
   int adc_reading;
   while(true)
     {
       adc_reading = read_analog(0);
       printf("Front: %d", adc_reading);
       adc_reading = read_analog(1);
-      printf("Left: %d", adc_reading);
+      printf("  Left: %d", adc_reading);
       adc_reading = read_analog(2);
-      printf("Right: %d\n", adc_reading);
-      Sleep(0,2500);
+      printf("  Right: %d\n", adc_reading);
+      Sleep(0,999999);
     }
 
 }
