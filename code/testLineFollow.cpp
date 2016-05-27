e4a21fd4b0bf60427c689cda80b499e35ce7dae8
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-27 15:00:48 +1200

    pixelthresh 105

--- code/testLineFollow.cpp
+++ code/testLineFollow.cpp
@@ -34,7 +34,7 @@ int main()
 	time_t end_t = 0; //End point for calculating time difference
 	bool lineFound;
 	bool centered;
-	int pixelThresh = 100;
+	int pixelThresh = 105;
 	while(true) //This creates a never ending loop
 	{
 		pTot = 0;
