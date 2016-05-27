5eb78231b3dcdc3e62872d33d2471d7818120b9b
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-27 17:26:13 +1200

    update kp and kd

--- code/testLineFollow.cpp
+++ code/testLineFollow.cpp
@@ -25,8 +25,8 @@ int main()
 	int left; 
 	int right;
 	int motorSpeed = 50;
-	double kp = 3; //P value in PD controller
-	double kd = 0.0005; //D value in PD controller
+	double kp = 2.5; //P value in PD controller
+	double kd = 0.0000; //D value in PD controller
 	double ki = 0;
 	double pixelCount;
 	double timeStep = 0.1; //The time period used for calculating kp
