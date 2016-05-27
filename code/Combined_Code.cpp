1beafde9412853144b3d8fd5678a71345953f6d7
Nick Lauder <nalauder@users.noreply.github.com>
2016-05-27 16:26:28 +1200

    returning network messaeg

--- code/Combined_Code.cpp
+++ code/Combined_Code.cpp
@@ -42,7 +42,7 @@ int main ()
 	connect_to_server("130.195.6.196", 1024); //connects to server with the ip address 130.195.6.196 on port 1024
 	send_to_server("Please"); //sends please to the connected server
 	receive_from_server(message); //receives message from the connected server
-	send_to_server(message);
+	send_to_server("%s", message);
 	while(true) //This creates a never ending loop
 	{
 		pTot = 0;
