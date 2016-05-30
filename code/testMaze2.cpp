65efff5b1ff1adb3fd6fcabdeacb2ff3a3558f4f
finn-wa <finnwa24@gmail.com>
2016-05-30 19:09:55 +1200

    intersection if statement

--- code/testMaze2.cpp
+++ code/testMaze2.cpp
@@ -20,6 +20,7 @@ init(0);
 	int motorSpeed = 50;
 	double left; //amount to adjust each motor speed
 	double right;
+	bool intersection = false;
 
         while(true){
         	frontIR = read_analog(0); //reads IR sensors and prints to the console
@@ -33,8 +34,9 @@ init(0);
 
                 if(frontIR > frontWall){ //if a wall is detected in front of the robot
                         printf("wall in front of robot detected \n");
-                        if((rightIR < rightWall) && (leftIR < leftWall)){ //turn right if there is no wa$
-                                printf("no wall left all right \n");
+                        if((rightIR < rightWall) && (leftIR < leftWall)){ //turn right if there is no wall
+                                printf("no wall left or right \n");
+                                intersection = true;
                 	        set_motor(1, -1.5  * motorSpeed); //tune 0.43 - got it from calc$
                                 set_motor(2, motorSpeed);
                                 
@@ -54,6 +56,15 @@ init(0);
                  //     set_motor(2, motorSpeed);
                 //}
           }else{ //if there is no wall in front of the robot
+                if(intersection){
+                	set_motor(1, 0.3* motorSpeed);
+                        set_motor(2, motorSpeed)
+                        if(rightIR > rightWall){
+                        	intersection = false;
+                        }
+                }
+                        
+                	
                 errorValue = (leftWall - leftIR)+(rightIR - rightWall); //generate negative error for to$
                                                                         //whether this translates to cor$
                 right = motorSpeed - (errorValue * kp); //find motor speeds
